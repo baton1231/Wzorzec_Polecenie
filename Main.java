3afb144a9b264e209dc462d76498c3cb263b8420
Michał Kochański <info@mkoch.pl>
2012-11-14 00:00:43 +0100

    LAST

--- Main.java
+++ Main.java
@@ -5,21 +5,21 @@ public class Main {
 		Zawodnik z1 = new Zawodnik("Kowalski");
 		Zawodnik z2 = new Zawodnik("Nowak");
 		Zawodnik z3 = new Zawodnik("Brzeczyszczykiewicz");
-		 
+
 		Bieganie bieganie = new Bieganie(z1);
 		Plywanie plywanie = new Plywanie(z3);
-		 
+
 		trener.setMode(bieganie);
 		trener.rozkaz();
 		trener.setMode(plywanie);
 		trener.rozkaz();
 		trener.cofnij();
-		 
+
 		System.out.println();
-		 
+
 		Polecenie []tab = { new Cwiczenie(z2), new Bieganie(z2), new Plywanie(z2)};
 		PelnyTrening pelnyTrening = new PelnyTrening(tab);
 		trener.setMode(pelnyTrening);
 		trener.rozkaz();
-		}
+	}
 }
